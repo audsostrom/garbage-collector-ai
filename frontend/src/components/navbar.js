f94b1babc03c3e4537841248ba28c205e1b13969
audsostrom <audreyostrom64@gmail.com>
2024-04-27 18:04:19 -0700

    navbar works and redirects

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -12,27 +12,55 @@ import {
 import logo from "../assets/recycle.svg";
 import menu from "../assets/menu-icon.svg";
 import "./navbar.css";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  const [openDrawer, setOpenDrawer] = useState(false);
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
 
-  const handleDrawerOpen = () => {
-    setOpenDrawer(true);
-  };
-
-  const handleDrawerClose = () => {
-    setOpenDrawer(false);
+  const toggleDrawer = () => {
+    setIsDrawerOpen(!isDrawerOpen);
   };
 
   return (
     <div className="navbar-container">
+      <Link to="/">
       <div className="left-side">
         <div className="logo-name">Trash.ai</div>
-        <img className="logo" src={logo}></img>
+        <img className="logo" src={logo} alt="logo" />
       </div>
-      <button className="right-side">
-        <img className="menu" src={menu} />
+      </Link>
+      <button className="right-side" onClick={toggleDrawer}>
+        <img className="menu" src={menu} alt="menu" />
       </button>
+      <Drawer
+        anchor="right"
+        open={isDrawerOpen}
+        onClose={toggleDrawer}
+      >
+        <div
+          role="presentation"
+          onClick={toggleDrawer}
+          onKeyDown={toggleDrawer}
+        >
+          <List>
+            <ListItem button key="Profile">
+              <ListItemText primary="Profile"/>
+            </ListItem>
+            <Link to="/upload">
+               <ListItem button key="Upload a Picture">
+               <ListItemText primary="Upload a Picture"/>
+               </ListItem>
+            </Link>
+            <ListItem button key="Waste Map">
+              <ListItemText primary="Waste Map"/>
+            </ListItem>
+            <ListItem button key="Leaderboards">
+              <ListItemText primary="Leaderboards"/>
+            </ListItem>
+            {/* Add more ListItem components for other menu items */}
+          </List>
+        </div>
+      </Drawer>
     </div>
   );
 };
