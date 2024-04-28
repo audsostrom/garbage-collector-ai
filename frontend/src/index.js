dc3fe5da224634f0bbc0b4b4112e583fdadd7892
audsostrom <audreyostrom64@gmail.com>
2024-04-27 19:09:38 -0700

    adding files

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { AuthProvider } from "@propelauth/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider authUrl={process.env.REACT_APP_AUTH_URL}>
+      <App />
+    </AuthProvider>,
   </React.StrictMode>,
 );
 
