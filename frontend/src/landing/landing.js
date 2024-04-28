256c6540022bae69ad49bf385a0600ba9d1f3304
audsostrom <audreyostrom64@gmail.com>
2024-04-27 18:37:00 -0700

    landing page done

--- frontend/src/landing/landing.js
+++ frontend/src/landing/landing.js
@@ -1,16 +1,10 @@
 import React, { useState } from "react";
-import {
-  AppBar,
-  Toolbar,
-  IconButton,
-  Typography,
-  Drawer,
-  List,
-  ListItem,
-  ListItemText,
-} from "@mui/material";
 import "./landing.css";
 import { useNavigate } from "react-router-dom";
+import frog from '../assets/frog-landing.svg';
+import topCircles from '../assets/upper-circles.svg';
+import lowerCircles from '../assets/lower-circles.svg';
+
 
 const Landing = () => {
   let navigate = useNavigate();
@@ -22,8 +16,21 @@ const Landing = () => {
 
   return (
     <div className="landing-container">
-      <div>Hi</div>
-      <button onClick={routeChange}>Take a Picture</button>
+      <div id="left-side-content">
+         <div className="landing-text">
+         <div className="header">Keep Things Better Than You Found It</div>
+         <div className="subheader">We empower communities to tackle litter and create a cleaner, healthier planet. <b>Trash.ai</b> is the one-stop app to report, clean up, and prevent litter. Simply take a picture of litter and upload it with your location. Our AI will identify the waste and suggest disposal methods.</div>
+
+
+         </div>
+
+      </div>
+      <div id="right-side-content">
+         <img className="frog-logo" src={frog}></img>
+
+         <button id="take-a-pic" onClick={routeChange}>Take a picture of trash!</button>
+
+      </div>
     </div>
   );
 };
